3aa9fb0433d11d0e70ba2be9b5488a5b2343dadf
WangAphisit Nattasan <ts-natta.wangaphisit@rakuten.com>
2022-04-05 17:01:29 +0900

    initial commit

---
+++ src/main/java/fun/challenge/ConnectFourMain.java
@@ -0,0 +1,11 @@
+package fun.challenge;
+
+import java.util.logging.Logger;
+
+public class ConnectFourMain {
+    private static final Logger logger = java.util.logging.Logger.getLogger(ConnectFourMain.class.getSimpleName());
+
+    public static void main(String[] args) {
+        logger.info("Hello World");
+    }
+}
